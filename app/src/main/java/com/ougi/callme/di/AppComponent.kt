d076e11af2d89f7fe4bed511dbdc20de20757030
comougi <comougi@gmail.com>
2024-05-12 15:03:58 +0300

    make components Singleton

--- app/src/main/java/com/ougi/callme/di/AppComponent.kt
+++ app/src/main/java/com/ougi/callme/di/AppComponent.kt
@@ -3,7 +3,9 @@ package com.ougi.callme.di
 import com.ougi.callme.di.modules.core.CoreModule
 import com.ougi.callme.presentation.App
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(
     modules = [
         CoreModule::class
--- app/src/main/java/com/ougi/callme/di/modules/core/network/NetworkModule.kt
+++ app/src/main/java/com/ougi/callme/di/modules/core/network/NetworkModule.kt
@@ -4,11 +4,13 @@ import com.ougi.networkapi.NetworkApi
 import com.ougi.networkimpl.di.NetworkComponentProvider
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 object NetworkModule {
 
     @Provides
+    @Singleton
     fun provideNetworkApi(): NetworkApi = NetworkComponentProvider.provide()
 
 }
